bb71b31b49b4a827033ee88a78654c9bae7262f6
Husniddin <norimqulovhusniddin@gmail.com>
2025-03-27 05:10:43 -0700

    SALOM2

--- src/pages/user.js
+++ src/pages/user.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "@/styles/sedap.css";
 import Menu from '@/components/Menu';
 import Link from 'next/link';
-
+import Image from 'next/image';
 
 function user() {
   return (
@@ -10,30 +10,30 @@ function user() {
         <div className='page'>
         <h1 className='sedap'>Sedap.</h1>
         <p className='modern'>Modern Admin Dashboard</p>
-    <Menu img="./home.png" name="Dashboard"/>
+    <Menu Image="./home.png" name="Dashboard"/>
     <Link href="http://localhost:3001/sedap"> <Menu img="./chiziqlar.png" name="Order List"/></Link>
-    <Menu  img="./papka.png" name="Order Detail"/>
-    <Menu  img="./user.png" name="Customer"/>
-    <Menu  img="./analiz.png" name="Analytics"/>
-    <Menu  img="./qalam.png" name="Reviews"/>
-    <Menu  img="./coffe.png" name="Foods"/>
-    <Menu  img="./note.png" name="Food Detail"/>
-    <Menu  img="./user.png" name="Customer Detail"/>
-    <Menu  img="./calendar.png" name="Calendar"/>
-    <Menu  img="./chat.png" name="Chat"/>
-    <Menu  img="./wallet.png" name="Wallet"/>
+    <Menu  Image="./papka.png" name="Order Detail"/>
+    <Menu  Image="./user.png" name="Customer"/>
+    <Menu  Image="./analiz.png" name="Analytics"/>
+    <Menu  Image="./qalam.png" name="Reviews"/>
+    <Menu  Image="./coffe.png" name="Foods"/>
+    <Menu  Image="./note.png" name="Food Detail"/>
+    <Menu  Image="./user.png" name="Customer Detail"/>
+    <Menu  Image="./calendar.png" name="Calendar"/>
+    <Menu  Image="./chat.png" name="Chat"/>
+    <Menu  Image="./wallet.png" name="Wallet"/>
     
-    <img src="./banner.png" alt="" />
+    <Image src="./banner.png" alt="" />
     <h4 className='res'>Sedap Restaurant Admin Dashboard</h4>
     <p className='ikki'>© 2020 All Rights Reserved</p>
     <p className='made'>Made with ❤️ by Peterdraw</p>
     </div>
     <div className='buts'>
     <input className='search' type="text" placeholder='Search here' />
-    <img className='logo' src="./qongiroq.png" alt="" />
-    <img className='logo' src="./com.png" alt="" />
-    <img className='logo' src="./box.png" alt="" />
-    <img className='logo' src="./setting.png" alt="" />
+    <Image className='logo' src="./qongiroq.png" alt="" />
+    <Image className='logo' src="./com.png" alt="" />
+    <Image className='logo' src="./box.png" alt="" />
+    <Image className='logo' src="./setting.png" alt="" />
     <h4 className='hello'>Hello, Samantha</h4>
     
 </div>
